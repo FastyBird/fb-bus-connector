bf058604e0a03784ac030ed861b516b31639d3e9
Adam Kadlec <adam.kadlec@gmail.com>
2022-02-10 21:44:47 +0100

    QA fixes

--- src/Entities/FbBusConnector.php
+++ src/Entities/FbBusConnector.php
@@ -15,7 +15,6 @@
 
 namespace FastyBird\FbBusConnector\Entities;
 
-use Consistence\Doctrine\Enum\EnumAnnotation as Enum;
 use Doctrine\ORM\Mapping as ORM;
 use FastyBird\DevicesModule\Entities as DevicesModuleEntities;
 use FastyBird\FbBusConnector\Types;
